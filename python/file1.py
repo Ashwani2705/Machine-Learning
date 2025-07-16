38dab5866b4278157c72ce04b9f5ffd6c49def80
Ashwani2705 <ashwaniraiabc@gmail.com>
2025-07-16 14:58:29 +0530

    append mode

--- python/file1.py
+++ python/file1.py
@@ -5,3 +5,18 @@ with open('python\check.txt','r') as file:
         print(line.rstrip())    # This removes trailing whitespace including newlines
         print(line.lstrip())    # This removes leading whitespace
            
+
+with open('python\check.txt','w') as file:
+    file.write("Hello world\n")
+    file.write("This is a new line ")
+
+#checking the content of the file 
+with open('python\check.txt','r') as file:
+    for line in file:
+        print(line)
+
+
+
+#Writing a file without overwriting 
+with open('python\check.txt','a') as file:    #opening file in append mode 
+    file.write("append operarion taking place lets see ")
