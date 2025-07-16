fadaadbec37a538dcc16b6efbb71969a0f367815
Ashwani2705 <ashwaniraiabc@gmail.com>
2025-07-16 14:38:45 +0530

    append in that only

--- python/file1.py
+++ python/file1.py
@@ -1,3 +1,7 @@
 with open('python\check.txt','r') as file:
     for line in file:
         print(line)
+        print(line.strip()) # THis removes leading and trailing whitespace including newlines
+        print(line.rstrip())    # This removes trailing whitespace including newlines
+        print(line.lstrip())    # This removes leading whitespace
+           
